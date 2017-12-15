b2b3a73b7a26227b4412f2cef37c2e89de810061
eastsea <ysjk2003@naver.com>
2017-12-15 17:21:42 +0900

    Add some

---
+++ Front/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/main.html")
+def hello():
+    return render_template('main.html')
+
+@app.route("/login.html")
+def login():
+    return render_template('login.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
